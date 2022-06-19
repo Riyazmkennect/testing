256ca1db8f77fe79a1ddbdab4efffd02d6eac27b
Syed Riyaz <106138842+Riyazmkennect@users.noreply.github.com>
2022-06-19 23:40:42 +0530

    Add files via upload

---
+++ nuxt.config.js
@@ -0,0 +1,85 @@
+mode:'spa'
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: '%s - poadcaster',
+    title: 'poadcaster',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    ['@nuxtjs/firebase',
+  {
+    config:{
+      apiKey: "AIzaSyDZuMgV4X0G4FJPiZcr3oKEyVQtwogg58E",
+      authDomain: "podcaster-67838.firebaseapp.com",
+      projectId: "podcaster-67838",
+      storageBucket: "podcaster-67838.appspot.com",
+      messagingSenderId: "335203664666",
+      appId: "1:335203664666:web:17b750a7a0a5140547a2a3",
+      measurementId: "G-RFWNMZ57P4"
+    },
+    services:{
+      auth:{
+        initialize:{
+        onAuthStateChangedMutation:'ON_AUTH_STATE_CHANGED_MUTATION',
+      }
+    }
+    }
+  }]
+  ],
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-container>
+        <h1>Welcome To  Podcast live <br/><span v-if="currentUser">{{currentUser.displayName}}</span></h1>
+
+        <v-card elevation="12" class="pa-3">
+            <signIn/>
+        </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import signIn from '../components/signIn.vue'
+export default {
+    computed:{
+     currentUser(){
+        return this.$store.state.user;
+     }
+    },
+  components:{
+    signIn
+  }
+}
+</script>
+
+<style>
+
+</style>
